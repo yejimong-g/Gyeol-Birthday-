7b9deda47ad046958b9c1cda68a8ae0064e7dc3d
yejimong-g <yejishin428@gmail.com>
2026-01-07 12:53:42 -0800

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,7 +10,7 @@ const About = () => {
       <Bar />
       <div className="font-kangwon-light text-xl space-y-8 flex flex-col items-center py-10 border-2 border-purple1 px-8 rounded-lg mt-8 bg-purple1/30">
         <span>* 사용된 기술 : React.js, Next.js, Typescript, TailwindCSS</span>
-        <span>* 개발 기간 : 2026. 1. 7. 하루!</span>
+        <span>* 개발 기간 : 202?. ??. ??. 몰라!</span>
         <span>* Made by 신예지</span>
         <button
           onClick={() =>
